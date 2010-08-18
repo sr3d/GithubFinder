c34e4d4165ee42db0779103dc2e2baf22ac8b58e
Alex Le <nworld3d@yahoo.com>
2010-08-18 04:46:38 -0500

    - working on the build script to build the release folder automatically - minify the png files into much smaller gif's - minify the index.app.html as the release index file
    still about 1.5KB over.  so will probably have to rewrite/optimize some stuff.

--- javascripts/bootstrap.js
+++ javascripts/bootstrap.js
@@ -1,27 +1,37 @@
-document.on('dom:loaded', function() { 
-  var img = new Image();
-  img.onload=function() {
-    var js        = "", 
-        canvas    = document.createElement("canvas"),
-        context   = canvas.getContext("2d"),
-        width     = img.width,
-        height    = img.height
+var f,d=document;
+d.on('dom:loaded', function() { 
+  var x = function(z, m ) {  /// image, callback
+    var o = new Image();
+    o.onload=function() {
+      var s = "", 
+          c = d.createElement("canvas"),
+          t = c.getContext("2d"),
+          w = img.width,
+          h = img.height
 
-    // debugger
-    canvas.width  = canvas.style.width  = width;
-    canvas.height = canvas.style.height = height; 
-    context.drawImage(img, 0, 0);
-    var data = context.getImageData( 0, 0, width, height ).data;
-    for(var i= 0, len = data.length; i < len; i += 4){
-      if( data[i] > 0 )
-       js += String.fromCharCode(data[i]);
+      debugger
+      c.width  = c.style.width  = w;
+      c.height = c.style.height = h; 
+      t.drawImage(o, 0, 0);
+      
+      var b = t.getImageData( 0, 0, w, h ).data; //b : bucket of data
+      for(var i= 0, l = b.length; i < l; i += 4){
+        if( b[i] > 0 )
+         s += String.fromCharCode(b[i]);
+      }
+      m(s);
     }
+    img.src = z;
+  }
 
-    eval( js );
-    
-    run();
-    
-  };
-
-  img.src = window.codePng;
+  /* execute */
+  x(codePng, function(s){
+    console.log(s);
+    eval(s); 
+    f = new F();
+  });
+  
+  x('css.png', function(s) { 
+    console.log("s %o",s);
+  });
 })
