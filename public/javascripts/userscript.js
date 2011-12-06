fc68428c7f84e5060a3aa27ca8d0057c41243f26
Alex Le <nworld3d@yahoo.com>
2011-12-06 11:43:50 -0800

    update to Github's changes

--- public/javascripts/userscript.js
+++ public/javascripts/userscript.js
@@ -6,7 +6,7 @@
       user      = path[0],
       repo      = path[1],
       branch    = path[3],  
-      li    = $('ul.actions li:first')[0],
+      li    = $('ul.pagehead-actions li:first')[0],
       html  = '<li><a href="' + ghf + 
                         '&user_id=' + user + 
                         '&repo=' + repo +'" target=_blank class="minibutton"><span>Browse in GHFinder</span></a></li>';
@@ -23,7 +23,7 @@
             '&user_id=' + repoInfo[1] +
             '&repo=' + repoInfo[2] + 
             '" target=_blank> ' + 
-              '<img src="http://github.com/sr3d/GithubFinder/raw/master/img/folder_explore.png"/>'
+              '<img src="https://github.com/sr3d/GithubFinder/raw/master/public/img/folder_explore.png"/>'
             '</a>';
     $(a).after(html);
   });
